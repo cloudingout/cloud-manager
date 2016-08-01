f1c5954ad9e696ca87b442c9fc1c6b8f6d91c0a3
davidcgarcia <davidg9404@gmail.com>
2016-08-01 15:58:38 +0200

    Generando archivo de configuracion

---
+++ Config/Config.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+  
+  'connections' => [
+
+    'mysql' => [
+      'driver'    => 'mysql', 
+      'host'      => 'localhost', 
+      'port'      => 3306, 
+      'database'  => 'cloud_manager', 
+      'username'  => 'root', 
+      'password'  => 'L4r4v3l*.', 
+      'charset'   => 'utf8', 
+      'collation' => 'utf8_unicode_ci', 
+      'prefix'    => '', 
+      'static'    => false, 
+      'engine'    => null
+    ],
+
+    'pgsql' => [
+
+    ],
+
+  ],
+
+];
