6db841aaa43ae33867e66871f31e0cb9ca907189
davidcgarcia <davidg9404@gmail.com>
2016-08-01 15:56:56 +0200

    Defiendo constantes utiles para nuestro proyecto

---
+++ Config/fig.php
@@ -0,0 +1,13 @@
+<?php 
+
+  /** 
+  * Definimos separador de directorios, ruta absoluta del archivo y 
+  * url http
+  *
+  */
+  define('DS', DIRECTORY_SEPARATOR);
+  define('ROOT', realpath(dirname(__FILE__)) . DS);
+  define('URL', 'http://server3.metin2renacer.com/');
+  define('ASSETS', '/cloud_manager/Public/assets/css/');
+  define('__SECRET_KEY__', 'azdfvbjkluhg@652.*!');
+  
