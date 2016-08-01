61e9c5ac8694bc83287ff009187a3f897893b57c
davidcgarcia <davidg9404@gmail.com>
2016-08-01 15:54:54 +0200

    Generamos archivo Request - encargado de asignar controladores a traves de la URL

---
+++ Config/Request.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Config;
+
+/** 
+* Obtiene la URL y a través de ella se asignan los controladores, métodos 
+* y argumentos (si los hay) 
+*
+* @package cloud_manager
+* @author Cristhian David García
+*/
+
+class Request 
+{
+  /**
+  * Obtenemos el controlador que será usado
+  *
+  * @var $controller
+  */
+  private $controller;
+
+  /** 
+  * Obtenemos el method que será usado
+  * 
+  * @var $method
+  */
+  private $method;
+
+  /**
+  * Obtenemos el argumento (si lo hay) que será usado
+  *
+  * @var $argument
+  */
+  private $argument;
+
+  /** 
+  * Constructor de la clase - a través de la url definimos los métodos, 
+  * controladores y/o argumentos que serán usados. 
+  *
+  * @return void
+  */
+  public function __construct()
+  {
+    if (isset($_GET['url'])) {
+      $url = filter_input(INPUT_GET, 'url', FILTER_SANITIZE_URL);
+      $url = explode('/', $url); 
+      $url = array_filter($url);
+
+      if ($url[0] == 'index.php') {
+        $this->controller = 'Auth';
+      } else {
+        $this->controller = ucwords(array_shift($url));
+      }
+
+      $this->method = strtolower(array_shift($url));
+
+      if (!$this->method) {
+        $this->method = 'index';
+      }
+
+      $this->argument = $url;
+    } else {
+      $this->controller = 'Auth';
+      $this->method = 'index';
+    }
+
+  }
+
+  /** 
+  * Obtiene el controlador 
+  *
+  * @return string $this->controller 
+  */
+  public function getController()
+  {
+    return $this->controller;
+  }
+
+  /** 
+  * Obtiene el método 
+  *
+  * @return string $this->method 
+  */
+  public function getMethod()
+  {
+    return $this->method;
+  }
+
+  /** 
+  * Obtiene el argumento 
+  *
+  * @return string $this->argument 
+  */
+  public function getArgument()
+  {
+    return $this->argument;
+  }
+
+}
