a7d6ce05ad047cecb432073e8ec4a30a01622348
davidcgarcia <davidg9404@gmail.com>
2016-08-17 19:15:24 +0200

    Controlador y vista para el home

---
+++ Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Controllers;
+use Models\user as user;
+
+class HomeController
+{
+
+
+  public function index()
+  {
+    //echo 'HomeController';
+  }
+}
---
+++ Resources/Views/home/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html class='no-js' lang='es'>
+  <head>
+    <meta charset='utf8'>
+    <title>Cloud Manager</title>
+    <meta content='Cloud manager, administración de servidores' name='description'>
+    <meta content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0' name='viewport'>
+    <link href='http://server3.metin2renacer.com/assets/css/normalize.css' rel='stylesheet'>
+    <link href='http://server3.metin2renacer.com/assets/css/flexboxgrid.css' rel='stylesheet'>
+    <link href='http://server3.metin2renacer.com/assets/css/main.css' rel='stylesheet'>
+    <script src='http://server3.metin2renacer.com/assets/js/modernizr.min.js'></script>
+  </head>
+  <body>
+    <!--[if lt IE 9]>
+    <p class='browserupgrade'>
+      Estas usando un navegador
+      <strong>desactualizado</strong>
+      Por favor
+      <a href='http://browsehappy.com/'>
+        actualiza tu navegador
+      </a>
+      para una mejor experiencia
+    </p>
+    <![endif]-->
+    <div class='video-container'>
+      <video autoplay height='720' loop poster='http://server3.metin2renacer.com/videos/imgs/Aloha-Mundo.jpg' width='120'>
+        <source src='http://server3.metin2renacer.com/videos/mp4/Aloha-Mundo.mp4' type='video/mp4'>
+        <source src='http://server3.metin2renacer.com/videos/webm/Aloha-Mundo.webm' type='video/webm'>
+      </video>
+    </div>
+  </body>
+</html>
