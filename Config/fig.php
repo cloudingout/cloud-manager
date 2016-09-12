1e715ee4bfa35a3d3ee4bb680ee4f07105374c22
luisito666 <luispenagos91@gmail.com>
2016-09-12 14:30:28 -0500

    modificando gitignore

--- Config/fig.php
+++ Config/fig.php
@@ -7,7 +7,7 @@
   */
   define('DS', DIRECTORY_SEPARATOR);
   define('ROOT', realpath(dirname(__FILE__)) . DS);
-  define('URL', 'http://server3.metin2renacer.com/');
-  define('ASSETS', 'http://server3.metin2renacer.com/assets/');
+  define('URL', 'http://app.local/');
+  define('ASSETS', 'http://app.local/assets/');
   define('__SECRET_KEY__', 'azdfvbjkluhg@652.*!');
   
